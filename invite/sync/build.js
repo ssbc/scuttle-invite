206fefc3fb4e13f57ee50e7770d9934df534c52e
Kieran <kieran.a.gibb@gmail.com>
2018-05-09 19:35:37 +1200

    perform invite.accept and test callback

--- invite/sync/build.js
+++ invite/sync/build.js
@@ -3,7 +3,10 @@ const getResponse = require('../../response/async/get')
 const Response = require('../../response/sync/build')
 
 module.exports = function Invite (server, msg) {
-  var self = msg.value.content
+  const { author, content } = msg.value
+  var self = content
+
+  self.id = msg.key
 
   self.isValid = () => {
     isInvite(self)
@@ -14,14 +17,14 @@ module.exports = function Invite (server, msg) {
   if (self.isValid()) {
 
     self.recipient = (cb) => {
-      if (!cb) return new Error("provide a callback")
+      if (!cb) throw new Error('provide a callback')
       new Promise((resolve, reject) => {
         // Do get recipient
         var recipient = null
         if (recipient) {
           resolve(recipient)
         } else {
-          var error = new Error("Missing recipient")
+          var error = new Error('Missing recipient')
           self.errors.push(error)
           reject(error)
         }
@@ -32,14 +35,14 @@ module.exports = function Invite (server, msg) {
     }
 
     self.response = (cb) => {
-      if (!cb) return new Error("provide a callback")
+      if (!cb) throw new Error('provide a callback')
       new Promise((resolve, reject) => {
         // Do get response
         var response = null
         if (response) {
           resolve(response)
         } else {
-          reject(new Error("Not yet responded"))
+          reject(new Error('Not yet responded'))
         }
       }).then(
         resp => cb(null, resp),
@@ -51,16 +54,27 @@ module.exports = function Invite (server, msg) {
       if (err) throw err
       if (canReply()) {
         self.accept = (cb) => {
-          if (!cb) return new Error("provide a callback")
+          var attrs = {
+            type: 'response',
+            version: self.version,
+            root: self.root,
+            branch: self.id,
+            accept: true,
+            recps: [whoami.id, author]
+          }
+          if (!cb) throw new Error('provide a callback')
           new Promise((resolve, reject) => {
-            server.publish({
-              response
-            },
-            [whoami.id, msg.author],
-            (err, resp) => {
-              if (err) reject(err)
-              else resolve(resp)
+            // callback returns null ???
+            server.private.publish(
+              attrs,
+              [whoami.id, author],
+            )
+            var response = new Response(server, {
+              key: '???',
+              value: { author: whoami.id, content: attrs }
             })
+            if (!response) reject(new Error('failed to publish'))
+            else resolve(response)
           }).then(
             resp => cb(null, resp),
             err => cb(err, null)
@@ -68,15 +82,19 @@ module.exports = function Invite (server, msg) {
         }
 
         self.reject = (cb) => {
-          if (!cb) return new Error("provide a callback")
+          if (!cb) throw new Error('provide a callback')
           new Promise((resolve, reject) => {
-            server.publish({
-
+            server.private.publish({
+              type: 'response',
+              version: self.version,
+              root: self.root,
+              branch: self.id,
+              accept: false
             },
             [whoami.id, msg.author],
-            (err, resp) => {
-              if (err) reject(err)
-              else resolve(resp)
+            resp => {
+              if (!resp) reject(new Error('failed to publish'))
+              else resolve(new Response(server, resp))
             })
           }).then(
             resp => cb(null, resp),
@@ -88,17 +106,17 @@ module.exports = function Invite (server, msg) {
       function canReply () {
         return self.recps.filter(recp => {
           return recp !== whoami.id &&
-            recp === msg.author
+            recp === author
         }).length > 0
       }
     })
 
     self.isAccepted = (cb) => {
-      if (!cb) return new Error("provide a callback")
+      if (!cb) throw new Error('provide a callback')
       new Promise((resolve, reject) => {
         self.response((err, response) => {
           if (err) reject(err)
-          resolve(response.accept)
+          else resolve(response.accept)
         })
       }).then(
         bool => cb(null, bool),
--- response/sync/build.js
+++ response/sync/build.js
@@ -1,18 +1,21 @@
 const { isResponse } = require('ssb-invites-schema')
 
 module.exports = function Response (server, msg, invite = null) {
-  var self = msg.value.content
+  const { author, content } = msg.value
+  var self = content
+  console.log(self)
 
   self.isValid = () => {
     isResponse(self)
+    self.errors = self.errors || []
     return self.errors.length == 0 ? true : false
   }
 
-  if (isValid()) {
+  // if (isValid()) {
+  //   self.invite = invite ? invite : (cb) => {
 
-    self.invite = invite ? invite : (cb) => {
+  //   }
+  // }
 
-    }
-
-  }
+  return self
 }
