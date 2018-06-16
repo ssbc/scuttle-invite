7cb895c81ffa7d577df34f129b6a80ecb955cb52
Kieran <kieran.a.gibb@gmail.com>
2018-06-16 17:46:37 +1200

    outline of api returned by ScuttleInvite

---
+++ methods.js
@@ -0,0 +1,21 @@
+const { isInvite, isResponse } = require('ssb-invites-schema')
+
+module.exports = {
+  invites: {
+    async: {
+      private: {
+        publish: require('./invites/async/private/publish'),
+        reply: require('./invites/async/private/reply')
+      },
+      publish: require('./invites/async/publish'),
+      reply: require('./invites/async/reply'),
+      getInvite: require('./invites/async/getInvite'),
+      getResponse: require('./invites/async/getResponse'),
+      canReply: require('./invites/async/canReply')
+    },
+    sync: {
+      isInvite: () => isInvite,
+      isResponse:  () => isResponse
+    }
+  }
+}
