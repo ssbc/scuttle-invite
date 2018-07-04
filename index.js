45cbdf02d63ae05b3e7ba500f83afa0a03ba210f
Kieran <kieran.a.gibb@gmail.com>
2018-07-04 10:51:31 +0100

    add scuttle-inject dependency

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const methods = require('./methods')
-const PLUGIN_DEPS = ['invites', 'private', 'query']
-const Inject = require('./lib/inject')
+const PLUGIN_DEPS = ['invites', 'private', 'backlinks']
+const Inject = require('scuttle-inject')
 
 module.exports = function (server, opts) {
   return Inject(server, methods, PLUGIN_DEPS)
