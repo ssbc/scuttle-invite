cf7d1c6a584290329fdb79c41c74d762c9d9a078
Kieran <kieran.a.gibb@gmail.com>
2018-07-30 12:44:50 +0100

    remove top-level invites namespace in the API

--- methods.js
+++ methods.js
@@ -1,25 +1,25 @@
 const { isInvite, isReply } = require('scuttle-invite-schema')
 
 module.exports = {
-  invites: {
-    async: {
-      private: {
-        publish: require('./invites/async/private/publish'),
-        reply: require('./invites/async/private/reply')
-      },
-      publish: require('./invites/async/publish'),
-      reply: require('./invites/async/reply'),
-      getInvite: require('./invites/async/getInvite'),
-      getReply: require('./invites/async/getReply'),
-      canReply: require('./invites/async/canReply'),
-      isAccepted: require('./invites/async/isAccepted')
+  async: {
+    private: {
+      publish: require('./invites/async/private/publish'),
+      reply: require('./invites/async/private/reply')
     },
-    pull: {
-      byRoot: require('./invites/pull/byRoot')
-    },
-    sync: {
-      isInvite: () => isInvite,
-      isReply:  () => isReply,
-    }
+    publish: require('./invites/async/publish'),
+    reply: require('./invites/async/reply'),
+    getInvite: require('./invites/async/getInvite'),
+    getReply: require('./invites/async/getReply'),
+    canReply: require('./invites/async/canReply'),
+    isAccepted: require('./invites/async/isAccepted')
+  },
+  pull: {
+    invitesByRoot: require('./invites/pull/invitesByRoot'),
+    repliesByRoot: require('./invites/pull/invitesByRoot'),
+    allByRoot: require('./invites/pull/allByRoot')
+  },
+  sync: {
+    isInvite: () => isInvite,
+    isReply:  () => isReply,
   }
 }
